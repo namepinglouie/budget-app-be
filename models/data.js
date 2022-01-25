8d5e8db4d19d14f6a536f49321e9300a128a784a
Name Ping Louie <namepinglouie@gmail.com>
2022-01-25 14:03:59 -0500

    changed date data

--- models/data.js
+++ models/data.js
@@ -1,42 +1,42 @@
 module.exports = [
     {
-      date: "April 1",
+      date: "2022-04-01",
       name: "Groceries",
       amount: -30,
       from: "Ideal"
     },
     {
-      date: "April 1",
+      date: "2022-04-01",
       name: "Shopping",
       amount: -47,
       from: "Target"
     },
     {
-      date: "April 2",
+      date: "2022-04-02",
       name: "Income",
       amount: 2000,
       from: "Google"
     },
     {
-      date: "April 5",
+      date: "2022-04-05",
       name: "Gas",
       amount: -25,
       from: "Shell Gas Station"
     },
     {
-      date: "April 6",
+      date: "2022-04-06",
       name: "Rent",
       amount: -700,
       from: "Pike International, LLC"
     },
     {
-      date: "April 8",
+      date: "2022-04-08",
       name: "Income",
       amount: 2000,
       from: "Google"
     },
     {
-      date: "April 8",
+      date: "2022-04-08",
       name: "Groceries",
       amount: -40,
       from: "Mr. Kiwi"
